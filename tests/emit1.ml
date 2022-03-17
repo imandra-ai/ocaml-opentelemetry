3006e293b0f866cdb6a6b5a8b18693a77cf36fbf
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-03-17 14:22:10 -0400

    better test

--- tests/emit1.ml
+++ tests/emit1.ml
@@ -1,33 +1,38 @@
 
 module T = Opentelemetry
-
+let spf = Printf.sprintf
 let (let@) f x = f x
 
 let run () =
   Printf.printf "collector is on %S\n%!" (Opentelemetry_client_ocurl.get_url());
   let i = ref 0 in
   while true do
-    let@ (tr,sp) = T.Trace.with_ ~service_name:"t1" "loop" in
-    Unix.sleepf 2.;
+    let@ (tr,sp) = T.Trace.with_ ~service_name:"t1" "outer.loop" ~attrs:["i", `Int !i] in
 
-    let gc = Gc.stat() in
-    T.Metrics.(
-      emit [
-        gauge ~name:"ocaml_opentracing.test.i" [int !i];
-        gauge ~name:"ocaml_opentracing.test.major_heap_words" [int gc.Gc.heap_words];
-        sum ~name:"ocaml_opentracing.test.minor_allocated" [float gc.Gc.minor_words];
-      ]);
+    for j=0 to 4 do
 
-    incr i;
+      let@ (tr,sp) = T.Trace.with_ ~service_name:"t1" ~attrs:["j", `Int j] "loop.inner" in
+      Unix.sleepf 2.;
 
-    begin
-      let@ _ = T.Trace.with_ ~kind:T.Span.Span_kind_client
-          ~trace_id:tr ~parent:sp "alloc" in
-      (* allocate some stuff *)
-      let _arr = Sys.opaque_identity @@ Array.make (25 * 25551) 42.0 in
-      ignore _arr;
-      Unix.sleepf 0.1;
-  end;
+      let gc = Gc.stat() in
+      T.Metrics.(
+        emit [
+          gauge ~name:"ocaml_opentracing.test.i" [int !i];
+          gauge ~name:"ocaml_opentracing.test.major_heap_words" [int gc.Gc.heap_words];
+          sum ~name:"ocaml_opentracing.test.minor_allocated" [float gc.Gc.minor_words];
+        ]);
+
+      incr i;
+
+      begin
+        let@ _ = T.Trace.with_ ~kind:T.Span.Span_kind_client
+            ~trace_id:tr ~parent:sp "alloc" in
+        (* allocate some stuff *)
+        let _arr = Sys.opaque_identity @@ Array.make (25 * 25551) 42.0 in
+        ignore _arr;
+        Unix.sleepf 0.1;
+      end;
+    done;
   done
 
 let () =
