aea549862b62336b1a93ce599db606be85811dfe
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-03-18 11:06:36 -0400

    lwt library

---
+++ src/lwt/opentelemetry_lwt.ml
@@ -0,0 +1,37 @@
+
+open Opentelemetry
+
+module Span_id = Span_id
+module Trace_id = Trace_id
+module Span = Span
+
+module Trace = struct
+  open Proto.Trace
+  include Trace
+
+  (** Emit asynchronously *)
+  let emit (spans:span list) : unit Lwt.t =
+    let fut, wake = Lwt.wait() in
+    let ils =
+      default_instrumentation_library_spans ~spans () in
+    let rs = default_resource_spans ~instrumentation_library_spans:[ils] () in
+    Collector.send_trace [rs]
+      ~over:(fun () -> Lwt.wakeup_later wake ())
+      ~ret:(fun () -> fut)
+end
+
+module Metrics = struct
+  open Proto.Metrics
+  include Metrics
+
+  (** Emit some metrics to the collector. *)
+  let emit (l:t list) : unit Lwt.t =
+    let fut, wake = Lwt.wait() in
+    let lm =
+      default_instrumentation_library_metrics ~metrics:l () in
+    let rm = default_resource_metrics
+        ~instrumentation_library_metrics:[lm] () in
+    Collector.send_metrics [rm]
+      ~over:(fun () -> Lwt.wakeup_later wake ())
+      ~ret:(fun () -> fut)
+end
