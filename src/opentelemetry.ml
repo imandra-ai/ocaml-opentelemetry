15511a751727375caf631d1c22589e4b48a842d3
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-03-24 14:02:45 -0400

    docs

--- src/opentelemetry.ml
+++ src/opentelemetry.ml
@@ -582,16 +582,16 @@ module Metrics = struct
 end
 
 
+(** Implementation of the W3C Trace Context spec
+
+    https://www.w3.org/TR/trace-context/
+*)
 module Trace_context = struct
-  (** Implementation of the W3C Trace Context spec
 
-      https://www.w3.org/TR/trace-context/
-   *)
-
+  (** The traceparent header
+      https://www.w3.org/TR/trace-context/#traceparent-header
+  *)
   module Traceparent = struct
-    (** The traceparent header
-        https://www.w3.org/TR/trace-context/#traceparent-header
-    *)
 
     let name = "traceparent"
 
@@ -599,14 +599,14 @@ module Trace_context = struct
 
         The values are of the form:
 
-            {version}-{trace_id}-{parent_id}-{flags}
+        {version}-{trace_id}-{parent_id}-{flags}
 
         For example:
 
-            00-4bf92f3577b34da6a3ce929d0e0e4736-00f067aa0ba902b7-01
+        00-4bf92f3577b34da6a3ce929d0e0e4736-00f067aa0ba902b7-01
 
         {flags} are currently ignored.
-     *)
+    *)
     let of_value str : (Trace_id.t * Span_id.t, string) result =
       let ( let* ) = Result.bind in
       let blit ~offset ~len ~or_ =
