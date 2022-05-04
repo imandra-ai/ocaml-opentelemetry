a718f049aaaa07cc464228a2750f57d775bb1310
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-05-04 12:44:32 -0400

    fix: ensure we can always produce random trace_id/span_id

--- src/client/common_.ml
+++ src/client/common_.ml
@@ -1,21 +1,24 @@
 module Atomic = Opentelemetry_atomic.Atomic
+include Opentelemetry.Lock
 
-let[@inline] (let@) f x = f x
+let[@inline] ( let@ ) f x = f x
 
-let debug_ = ref (match Sys.getenv_opt "OTEL_OCAML_DEBUG" with Some ("1"|"true") -> true | _ -> false)
+let debug_ =
+  ref
+    (match Sys.getenv_opt "OTEL_OCAML_DEBUG" with
+    | Some ("1" | "true") -> true
+    | _ -> false)
 
-let lock_ : (unit -> unit) ref = ref ignore
-let unlock_ : (unit -> unit) ref = ref ignore
+let default_url = "http://localhost:4318"
 
-let set_mutex ~lock ~unlock : unit =
-  lock_ := lock;
-  unlock_ := unlock
+let url =
+  ref (try Sys.getenv "OTEL_EXPORTER_OTLP_ENDPOINT" with _ -> default_url)
 
-(* critical section for [f()] *)
-let[@inline] with_lock_ f =
-  !lock_();
-  Fun.protect ~finally:!unlock_ f
+let get_url () = !url
 
+let set_url s = url := s
+
+(** [with_mutex m f] calls [f()] in a section where [m] is locked. *)
 let[@inline] with_mutex_ m f =
   Mutex.lock m;
   Fun.protect ~finally:(fun () -> Mutex.unlock m) f
@@ -25,11 +28,21 @@ let parse_headers s =
   String.split_on_char ',' s |> List.map parse_header
 
 let default_url = "http://localhost:4318"
+
 let default_headers = []
-let url = ref (try Sys.getenv "OTEL_EXPORTER_OTLP_ENDPOINT" with _ -> default_url)
-let headers = ref (try parse_headers (Sys.getenv "OTEL_EXPORTER_OTLP_HEADERS") with _ -> default_headers)
+
+let url =
+  ref (try Sys.getenv "OTEL_EXPORTER_OTLP_ENDPOINT" with _ -> default_url)
+
+let headers =
+  ref
+    (try parse_headers (Sys.getenv "OTEL_EXPORTER_OTLP_HEADERS")
+     with _ -> default_headers)
+
 let get_url () = !url
+
 let set_url s = url := s
 
 let get_headers () = !headers
+
 let set_headers s = headers := s
---
+++ src/rand_bytes.mli
@@ -0,0 +1,9 @@
+val rand_bytes_16 : (unit -> bytes) ref
+(** Generate 16 bytes of random data *)
+
+val rand_bytes_8 : (unit -> bytes) ref
+(** Generate 16 bytes of random data *)
+
+val default_rand_bytes_8 : unit -> bytes
+
+val default_rand_bytes_16 : unit -> bytes
