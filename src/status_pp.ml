b826f859774ba6598953ebdadf4951350599a68d
Simon Cruanes <simon.cruanes.2007@m4x.org>
2023-11-07 15:33:05 -0500

    move protobuf code to opentelemetry.proto

--- src/status_pp.ml
+++
@@ -1,9 +0,0 @@
-[@@@ocaml.warning "-27-30-39"]
-
-let rec pp_status fmt (v:Status_types.status) = 
-  let pp_i fmt () =
-    Pbrt.Pp.pp_record_field ~first:true "code" Pbrt.Pp.pp_int32 fmt v.Status_types.code;
-    Pbrt.Pp.pp_record_field ~first:false "message" Pbrt.Pp.pp_bytes fmt v.Status_types.message;
-    Pbrt.Pp.pp_record_field ~first:false "details" (Pbrt.Pp.pp_list Pbrt.Pp.pp_bytes) fmt v.Status_types.details;
-  in
-  Pbrt.Pp.pp_brk pp_i fmt ()
