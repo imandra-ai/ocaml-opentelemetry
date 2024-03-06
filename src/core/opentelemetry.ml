1686f3825b6a7d1ff466093ba6dea8918f32bbcd
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-03-06 12:22:39 -0500

    some HTTP semantic conventions

--- src/core/opentelemetry.ml
+++ src/core/opentelemetry.ml
@@ -512,6 +512,26 @@ module Conventions = struct
 
       let version = "service.version"
     end
+
+    module HTTP = struct
+      let error_type = "error.type"
+
+      let request_method = "http.request.method"
+
+      let route = "http.route"
+
+      let url_full = "url.full"
+
+      (** HTTP status code, int *)
+      let response_status_code = "http.response.status_code"
+
+      let server_address = "server.address"
+
+      let server_port = "server.port"
+
+      (** http or https *)
+      let url_scheme = "url.scheme"
+    end
   end
 
   module Metrics = struct
@@ -530,6 +550,31 @@ module Conventions = struct
             let minor_collections = "process.runtime.ocaml.gc.minor_collections"
           end
         end
+      end
+    end
+
+    (** https://opentelemetry.io/docs/specs/semconv/http/ *)
+    module HTTP = struct
+      module Server = struct
+        let request_duration = "http.server.request.duration"
+
+        let active_requests = "http.server.active_requests"
+
+        (** Histogram *)
+        let request_body_size = "http.server.request.body.size"
+
+        (** Histogram *)
+        let response_body_size = "http.server.response.body.size"
+      end
+
+      module Client = struct
+        let request_duration = "http.client.request.duration"
+
+        (** Histogram *)
+        let request_body_size = "http.client.request.body.size"
+
+        (** Histogram *)
+        let response_body_size = "http.client.response.body.size"
       end
     end
   end
