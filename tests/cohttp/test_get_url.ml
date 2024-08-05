8aafa45896fad92aebd6d477cd18751b66beec7a
Corentin Leruth <corentin.leruth@gmail.com>
2024-08-05 10:04:12 +0200

    port urls test + rename

--- tests/cohttp/test_get_url.ml
+++
@@ -1,8 +0,0 @@
-let url = "http://localhost:3000"
-
-let cohttp () =
-  let config = Opentelemetry_client_cohttp_lwt.Config.make ~url () in
-  Opentelemetry_client_cohttp_lwt.with_setup ~config () @@ fun () ->
-  print_endline @@ Printf.sprintf "cohttp url = %s" url
-
-let () = cohttp ()
