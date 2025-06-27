d0f114bfd968d71e7ba9725332b1ad12804890cc
Shon Feder <shon.feder@gmail.com>
2025-06-26 20:19:51 -0400

    Switch to expect-based testing

--- tests/client_e2e/test_client_e2e_lwt.ml
+++ tests/client_e2e/test_client_e2e_lwt.ml
@@ -1,42 +1,42 @@
-let test = Test_server_lib.Tests.test
+(* let test = Test_server_lib.Tests.test *)
 
-module Client = Opentelemetry_client
-open Client.Signal
+(* module Client = Opentelemetry_client *)
+(* open Client.Signal *)
 
-(* Max batch size for the different signals  *)
-let batch_traces = 2
+(* (\* Max batch size for the different signals  *\) *)
+(* let batch_traces = 2 *)
 
-let ( let* ) = Option.bind
+(* let ( let* ) = Option.bind *)
 
-let ( let+ ) x f = Option.map f x
+(* let ( let+ ) x f = Option.map f x *)
 
-open Alcotest
+(* open Alcotest *)
 
 (* TODO: Running with batch-traces = 1 causes deadlocks *)
-let () =
-  Test_server_lib.run_tests
-    ~program_to_test:
-      [|
-        "emit1_cohttp"; "-j"; "2"; "--batch-traces"; string_of_int batch_traces;
-      |]
-    ~suite_name:"lwt-cohttp client"
-    [
-      test ~name:"WIP" (fun ts ->
-          let+ ts = to_traces ts in
-          fun () ->
-            check' int ~msg:"batch is of configured size" ~actual:batch_traces
-              ~expected:(List.length ts);
-            let id =
-              (((List.nth ts 0).scope_spans |> Fun.flip List.nth 0).spans
-             |> Fun.flip List.nth 0)
-                .trace_id
-            in
-            Format.eprintf ">>> %a@." Pp.traces ts;
-            Format.eprintf ">>> TRACE ID: %a@." Pbrt.Pp.pp_bytes id;
-            Format.eprintf ">>> FOOO";
-            Alcotest.fail "Dbg");
-      (* test ~name:"Fails" *)
-      (*   ~condition:(function *)
-      (*     | _ -> true) *)
-      (*   (fun _ -> Alcotest.fail "Failing"); *)
-    ]
+(* let () = *)
+(*   Test_server_lib.run_tests *)
+(*     ~program_to_test: *)
+(*       [| *)
+(*         "emit1_cohttp"; "-j"; "2"; "--batch-traces"; string_of_int batch_traces; *)
+(*       |] *)
+(*     ~suite_name:"lwt-cohttp client" *)
+(*     [ *)
+(*       test ~name:"WIP" (fun ts -> *)
+(*           let+ ts = to_traces ts in *)
+(*           fun () -> *)
+(*             check' int ~msg:"batch is of configured size" ~actual:batch_traces *)
+(*               ~expected:(List.length ts); *)
+(*             let id = *)
+(*               (((List.nth ts 0).scope_spans |> Fun.flip List.nth 0).spans *)
+(*              |> Fun.flip List.nth 0) *)
+(*                 .trace_id *)
+(*             in *)
+(*             Format.eprintf ">>> %a@." Pp.traces ts; *)
+(*             Format.eprintf ">>> TRACE ID: %a@." Pbrt.Pp.pp_bytes id; *)
+(*             Format.eprintf ">>> FOOO"; *)
+(*             Alcotest.fail "Dbg"); *)
+(*       (\* test ~name:"Fails" *\) *)
+(*       (\*   ~condition:(function *\) *)
+(*       (\*     | _ -> true) *\) *)
+(*       (\*   (fun _ -> Alcotest.fail "Failing"); *\) *)
+(*     ] *)
--- tests/client_e2e/trace_collector.ml
+++ tests/client_e2e/trace_collector.ml
@@ -8,15 +8,59 @@ let () = Logs.set_reporter (Logs_fmt.reporter ())
 
 and () = Logs.Src.set_level Cohttp_eio.src (Some Warning)
 
+module Tests = struct
+  type t = {
+    name: string;
+    test: Signal.t -> (unit -> unit) option;
+        (** [test s] should be [Some test] iff [test] is meant to be run on the
+            signal [s] *)
+  }
+
+  let test ~name test = { name; test }
+
+  let derive_alcotest tests signals : unit Alcotest.test_case list =
+    let rec find_tests conditional_tests runnable_tests =
+      let signal = Eio.Stream.take signals in
+      match conditional_tests with
+      | [] ->
+        (* We collected all the tests *)
+        runnable_tests
+      | rest ->
+        let test_cases, conditionals =
+          rest
+          |> List.partition_map (fun t ->
+                 match t.test signal with
+                 | Some test -> Left (Alcotest.test_case t.name `Quick test)
+                 | None -> Right t)
+        in
+        find_tests conditionals (test_cases @ runnable_tests)
+    in
+    find_tests tests [] |> List.rev
+
+  let run suite_name tests clock signals () =
+    (* Ensure server is up *)
+    Eio.Time.sleep clock 0.5;
+
+    let tests = derive_alcotest tests signals in
+
+    (* If we are not able to derive and run the tests within this limit, it
+       probably means the test program is not producing all the signals needed by
+       our tests  *)
+    let timeout_limit = 10. in
+    Eio.traceln "running test with timeout ";
+    Eio.Time.with_timeout_exn clock timeout_limit (fun () ->
+        Alcotest.run "client_e2e tests" [ suite_name, tests ])
+end
+
 (* Record and output events observed by the server *)
 module Record = struct
   let traceln_request kind req pp data =
     let _ = kind, req, pp, data in
     (****** NOTE: Uncomment for debugging *)
-    (* let () = *)
-    (*   Eio.traceln "# received %s\nREQUEST: %a\nBODY: %a\n@." kind *)
-    (*     Http.Request.pp req pp data *)
-    (* in *)
+    let () =
+      Eio.traceln "# received %s\nREQUEST: %a\nBODY: %a\n@." kind
+        Http.Request.pp req pp data
+    in
     ()
 
   let traces requests req data =
@@ -57,19 +101,20 @@ module Server = struct
 
   let log_warning ex = Logs.warn (fun f -> f "%a" Eio.Exn.pp ex)
 
-  let run ~env ~sw ~port requests () =
-    let socket =
-      Eio.Net.listen env#net ~sw ~backlog:128 ~reuse_addr:true
-        (`Tcp (Eio.Net.Ipaddr.V4.loopback, port))
-    and server = Cohttp_eio.Server.make ~callback:(handler requests) () in
-    Cohttp_eio.Server.run socket server ~on_error:log_warning
+  let run socket requests () =
+    Eio.traceln "starting server";
+    try
+      let server = Cohttp_eio.Server.make ~callback:(handler requests) () in
+      let () = Cohttp_eio.Server.run socket server ~on_error:log_warning in
+      `Stop_daemon
+    with Sys_error err ->
+      Eio.traceln "Caught sys error %s" err;
+      `Stop_daemon
 end
 
 (** Manage launching and cleaning up the program we are testing *)
 module Tested_program = struct
-  let cleanup proc =
-   fun () ->
-    match Unix.close_process proc with
+  let validate_exit = function
     | Unix.WEXITED 0 -> ()
     | Unix.WEXITED bad_code ->
       failwith
@@ -82,56 +127,28 @@ module Tested_program = struct
       failwith
       @@ Printf.sprintf "process under test ended with unexpected stop %d" i
 
+  let cleanup proc =
+   fun () ->
+    Eio.traceln "waiting for proc to end";
+    Unix.close_process proc |> validate_exit
+
   let run ~sw cmd () =
     let program = cmd.(0) in
+    Eio.traceln "opening proc";
     let proc = Unix.open_process_args program cmd in
     let pid = Unix.process_pid proc in
     at_exit (fun () -> Unix.kill pid 9);
-    Eio.Switch.on_release sw (cleanup proc)
-end
-
-module Tests = struct
-  type t = {
-    name: string;
-    test: Signal.t -> (unit -> unit) option;
-        (** [test s] should be [Some test] iff [test] is meant to be run on the
-            signal [s] *)
-  }
-
-  let test ~name test = { name; test }
-
-  let derive_alcotest tests signals : unit Alcotest.test_case list =
-    let rec find_tests conditional_tests runnable_tests =
-      let signal = Eio.Stream.take signals in
-      match conditional_tests with
-      | [] ->
-        (* We collected all the tests *)
-        runnable_tests
-      | rest ->
-        let test_cases, conditionals =
-          rest
-          |> List.partition_map (fun t ->
-                 match t.test signal with
-                 | Some test -> Left (Alcotest.test_case t.name `Quick test)
-                 | None -> Right t)
-        in
-        find_tests conditionals (test_cases @ runnable_tests)
+    Eio.Switch.on_release sw (cleanup proc);
+    let rec loop () =
+      match In_channel.input_line (fst proc) with
+      | Some l ->
+        Eio.traceln "pid (stdout): %d %s" pid l;
+        loop ()
+      | None -> ()
     in
-    find_tests tests [] |> List.rev
-
-  let run suite_name tests clock signals () =
-    (* Ensure server is up *)
-    Eio.Time.sleep clock 0.5;
-
-    let tests = derive_alcotest tests signals in
-
-    (* If we are not able to derive and run the tests within this limit, it
-       probably means the test program is not producing all the signals needed by
-       our tests  *)
-    let timeout_limit = 10. in
-    Eio.traceln "running test without timeout ";
-    Eio.Time.with_timeout_exn clock timeout_limit (fun () ->
-        Alcotest.run "client_e2e tests" [ suite_name, tests ])
+    Eio.traceln "pid (stdout): %d done reading" pid;
+    loop ();
+    Unix.waitpid [] pid |> snd |> validate_exit
 end
 
 let default_port =
@@ -141,14 +158,129 @@ let default_port =
   | _ -> failwith "unexpected format in Client.Config.default_url"
 
 let collect_traces ~env ~sw ~port program_to_test signals () =
-  Eio.Fiber.both
-    (Server.run ~env ~sw ~port signals)
-    (Tested_program.run ~sw program_to_test)
+  (* Create socket first, ensuring it will be available for tested program's collector *)
+  let socket =
+    Eio.Net.listen env#net ~sw ~backlog:128 ~reuse_addr:true
+      (`Tcp (Eio.Net.Ipaddr.V4.loopback, port))
+  in
+  Eio.Fiber.fork_daemon ~sw (Server.run socket signals);
+  let proc_mgr = Eio.Stdenv.process_mgr env in
+  Eio.Process.run ~stdout:(Eio.Stdenv.stderr env) proc_mgr program_to_test
 
-let run_tests ?(port = default_port) ~program_to_test ~suite_name tests =
+let take_all (s : 'a Eio.Stream.t) : 'a list =
+  let rec loop ls =
+    match Eio.Stream.take_nonblocking s with
+    | None -> List.rev ls
+    | Some x -> loop (x :: ls)
+  in
+  loop []
+
+(* let compare_signals a b = *)
+(*   match a, b with *)
+(*   | Signal.Logs _, Signal.Logs _ *)
+(*   | Signal.Metrics _, Signal.Metrics _ *)
+(*   | Signal.Traces _, Signal.Traces _ -> 0 *)
+(*   | Signal.Logs _, _ -> -1 *)
+(*   | Signal.Metrics,  *)
+
+(* type event = { *)
+(*   name: string; *)
+(*   attributes: (string * string) list; *)
+(* } *)
+
+(* type data_point = { *)
+(*   value: string; *)
+(*   attributes: (string * string) list; *)
+(* } *)
+
+(* type span_data = *)
+(*   { *)
+(*   name: string; *)
+(*   attributes: (string * string) list; *)
+(*   events: event list *)
+(*   } *)
+
+(* type scope_data =  { *)
+(*   scope: string; *)
+(*   spans: span_data list *)
+(* } *)
+
+(* type trace_data = *)
+(*   { *)
+(*     resource: string; *)
+(*     spans : span_data list *)
+(*   } *)
+
+(* type signal_data = *)
+(*   | Trace of trace_data *)
+(*   | Log of { *)
+(*       body: string; *)
+(*       severity: string; *)
+(*       attributes: (string * string) list; *)
+(*     } *)
+(*   | Metrics of { *)
+(*       scope: string; *)
+(*       name: string; *)
+(*       description: string; *)
+(*       unit: string; *)
+(*       data_points: data_point list; *)
+(*       is_monotonic: bool; *)
+(*     } *)
+
+(* let trace_to_trace_data : Proto.Trace.resource_spans -> trace_data = fun spans -> *)
+(*   let resource = spans.resource |> Option.fold ~some:(Format.asprintf "%a" Proto.Resource.pp_resource) ~none:"" in *)
+(*   let span_data : span_data = *)
+(*     spans.scope_spans |> List.map (fun s -> *)
+(*         { name : s.scope |> Option.fold ~some:(fun scope -> scope.name) ~none:"" *)
+(*         ; a} *)
+(*       ) *)
+
+(* let signals_to_signal_data : Signal.t -> signal_data list = function *)
+(*   | Traces ts -> *)
+(*     let scope = List.nth ts 0 in *)
+(*     {} *)
+
+let normalize_scope_span : Proto.Trace.scope_spans -> Proto.Trace.scope_spans =
+  function
+  | scope_span ->
+    {
+      scope_span with
+      spans =
+        scope_span.spans
+        |> List.map (fun (span : Proto.Trace.span) ->
+               {
+                 span with
+                 start_time_unix_nano = -1L;
+                 end_time_unix_nano = -1L;
+               });
+    }
+
+let normalize_signal : Signal.t -> Signal.t = function
+  | Traces ts ->
+    Traces
+      (ts
+      |> List.map (fun (trace : Proto.Trace.resource_spans) ->
+             {
+               trace with
+               scope_spans = trace.scope_spans |> List.map normalize_scope_span;
+             }))
+  | x -> x
+
+(* normalize trace output by redacting non-deterministic values from output *)
+let normalize =
+  let re =
+    Str.regexp
+      {|\(start_time_unix_nano\|time_unix_nano\|end_time_unix_nano\|value\) = \([0-9]*\|As_int([0-9]*)\|As_double([0-9]*\.)\);|}
+  in
+  fun s -> Str.global_replace re {|\1 = <redacted>;|} s
+
+let run ?(port = default_port) ~program_to_test () =
   Eio_main.run @@ fun env ->
-  Eio.Switch.run @@ fun sw ->
+  Eio.Switch.run ~name:"main" @@ fun _sw ->
   let signals = Eio.Stream.create max_int in
-  Eio.Fiber.first
-    (collect_traces ~env ~sw ~port program_to_test signals)
-    (Tests.run suite_name tests env#clock signals)
+  ( Eio.Switch.run ~name:"collector" @@ fun collector_sw ->
+    collect_traces ~env ~sw:collector_sw ~port program_to_test signals () );
+  take_all signals
+  |> List.map (fun s -> s |> Format.asprintf "%a" Signal.Pp.pp |> normalize)
+  |> List.stable_sort String.compare (* Produce a deterministic order *)
+  |> List.iter print_string
