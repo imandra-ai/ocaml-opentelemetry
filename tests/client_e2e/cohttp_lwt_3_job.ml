8b84025dea1a0ac83aa918da9f5d4244e545c014
Shon Feder <shon.feder@gmail.com>
2025-06-26 21:08:54 -0400

    Add test with 3 jobs

---
+++ tests/client_e2e/cohttp_lwt_3_job.ml
@@ -0,0 +1,15 @@
+let () =
+  Trace_collector.run
+    ~program_to_test:
+      [
+        "emit1_cohttp";
+        "-j";
+        "1";
+        "--batch-traces";
+        "2";
+        "--batch-metrics";
+        "2";
+        "--batch-logs";
+        "2";
+      ]
+    ()
