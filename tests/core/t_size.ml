2a2baeb7cd33a01a20118acf1069b9347e872bcc
Simon Cruanes <simon.cruanes.2007@m4x.org>
2025-10-30 22:19:39 -0400

    update test core

--- tests/core/t_size.ml
+++ tests/core/t_size.ml
@@ -2,20 +2,27 @@
 
 open Opentelemetry
 
-let res1 =
-  Metrics.make_resource_metrics
-    [
-      Metrics.sum ~name:"sum.foo"
-        [
-          Metrics.int ~start_time_unix_nano:42L ~now:45L 10;
-          Metrics.int ~start_time_unix_nano:52L ~now:55L 20;
-        ];
-      Metrics.gauge ~name:"gauge.bar"
-        [
-          Metrics.float ~start_time_unix_nano:42L ~now:45L 10.;
-          Metrics.float ~start_time_unix_nano:52L ~now:55L 20.;
-        ];
-    ]
+let res1 : Proto.Metrics.resource_metrics =
+  Proto.Metrics.make_resource_metrics ~schema_url:""
+    ~scope_metrics:
+      [
+        Proto.Metrics.make_scope_metrics ~schema_url:""
+          ~metrics:
+            [
+              Metrics.sum ~name:"sum.foo"
+                [
+                  Metrics.int ~start_time_unix_nano:42L ~now:45L 10;
+                  Metrics.int ~start_time_unix_nano:52L ~now:55L 20;
+                ];
+              Metrics.gauge ~name:"gauge.bar"
+                [
+                  Metrics.float ~start_time_unix_nano:42L ~now:45L 10.;
+                  Metrics.float ~start_time_unix_nano:52L ~now:55L 20.;
+                ];
+            ]
+          ();
+      ]
+    ()
 
 let str =
   let enc = Pbrt.Encoder.create () in
