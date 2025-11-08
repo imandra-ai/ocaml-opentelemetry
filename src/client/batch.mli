42e41675b576b669fce68c07ee65bbe03605981d
Corentin Leruth <corentin.leruth@gmail.com>
2025-11-08 12:43:21 +0100

    fmt

--- src/client/batch.mli
+++ src/client/batch.mli
@@ -47,6 +47,6 @@ val pop_if_ready : ?force:bool -> now:Mtime.t -> 'a t -> 'a list option
       batches before exit or because the user asks for it *)
 
 val push : 'a t -> 'a list -> [ `Dropped | `Ok ]
-(** [push b xs] is [`Ok] if it succeeds in pushing the values in [xs] into the batch
-    [b], or [`Dropped] if the current size of the batch has exceeded the high water
-    mark determined by the [batch] argument to [{!make}]. ) *)
+(** [push b xs] is [`Ok] if it succeeds in pushing the values in [xs] into the
+    batch [b], or [`Dropped] if the current size of the batch has exceeded the
+    high water mark determined by the [batch] argument to [{!make}]. ) *)
